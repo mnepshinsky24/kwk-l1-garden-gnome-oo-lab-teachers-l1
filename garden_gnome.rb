ef9e4580ce59c675dfd4e14ec05d2e6b4a5b8462
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-17 20:06:21 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,34 @@
 # Code your instances here
+
+class GardenGnome 
+  
+  def initialize(personality = "evil") 
+    @personality = "evil"
+    @hat_color
+  end
+  
+  def name=(name)
+    @name = name
+  end 
+  
+  def name
+    @name 
+  end
+  
+  def age=(age)
+    @age = age
+  end
+  
+  def age
+    @age
+  end 
+  
+    def gluten_allergy=(allergy)
+      @gluten_allergy = allergy 
+    end 
+    
+    def gluten_allergy
+      @gluten_allergy
+    end
+end 
+
